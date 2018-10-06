e843389ce6a6922b64aaa45fa34cd700582d788a
Kevin Schmid <kevin.schmid@edu.fh-joanneum.at>
2018-10-06 13:29:07 +0200

    added query param reading

--- ue1/server.js
+++ ue1/server.js
@@ -2,17 +2,19 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer(function(req,res) {
-    const urlObj = url.parse(req.url);
+    const urlObj = url.parse(req.url, true);
+
     if (urlObj.pathname === '/') {
         res.write('Hello World!');
     }
     else if (urlObj.pathname === '/hello') {
+        const nameParam = urlObj.query.name;
         res.write(`
 
 <html>
 <head></head>
 <body>
-    <h1>Hello!</h1>
+    <h1>Hello ${nameParam}!</h1>
 </body>
 </html>
 
