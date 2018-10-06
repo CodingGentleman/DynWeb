8da2be6209812cbc498649a7a276e28910721754
Kevin Schmid <kevin.schmid@edu.fh-joanneum.at>
2018-10-06 13:08:15 +0200

    added 404

--- ue1/server.js
+++ ue1/server.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function(req,res) {
-    res.write('Hello World!');
-    res.end();
+    const urlObj = url.parse(req.url);
+    if(urlObj.pathname === '/') {
+        res.write('Hello World!');
+        res.end();
+    }
+    else {
+        res.write('404 Not found!');
+        res.end();
+    }
+
 });
 
 server.listen(3000);
